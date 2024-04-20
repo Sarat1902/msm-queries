05dc2275409a970d9aa267d4dcfdad633f9d461d
Sarat1902 <161099567+Sarat1902@users.noreply.github.com>
2024-04-20 03:46:37 +0000

    commit

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,6 +5,26 @@ class DirectorsController < ApplicationController
   end
 
   def show
+    the_id = params.fetch("the_id")
+    matching_director = Director.where({ :id => the_id})
+    @the_dir = matching_director.at(0)
     render ({:template => "director_templates/details"})
   end
+
+  def junior
+    @director = Director.where.not({ :dob => nil }) 
+    @dob = @director.max_by(&:dob)
+    time = Time.new(@dob.dob.year, @dob.dob.month, @dob.dob.day)
+    @formatted_time = time.strftime("%B %e%t, %Y")
+    render ({:template => "director_templates/youngest"})
+  end
+
+  def senior
+    @director = Director.where.not({ :dob => nil }) 
+    @dob = @director.min_by(&:dob)
+    time = Time.new(@dob.dob.year, @dob.dob.month, @dob.dob.day)
+    @formatted_time = time.strftime("%B %e%t, %Y")
+    render ({:template => "director_templates/eldest"})
+  end
+
 end
